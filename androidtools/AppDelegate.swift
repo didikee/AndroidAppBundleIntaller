269b5236f1677fc50108314a71b70af2d29dd97c
didikeeLuanon <wwh@lunaon.com>
2020-07-21 18:10:33 +0800

    Initial Commit

---
+++ androidtools/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  androidtools
+//
+//  Created by didikee on 7/21/20.
+//  Copyright © 2020 didikee. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ androidtools/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  androidtools
+//
+//  Created by didikee on 7/21/20.
+//  Copyright © 2020 didikee. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
